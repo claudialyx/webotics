e59149a2948e6af93d757db3cb47483faefbefbc
Claudia <claudialyx@gmail.com>
2019-03-15 15:10:36 +0800

    linked to localhost

--- NLP/test.py
+++ NLP/test.py
@@ -1,25 +1,29 @@
 import spacy
+<<<<<<< HEAD
 
 from NLP.server import brain_folder_name
 
 input_dir = brain_folder_name
+=======
+input_dir = "C:\\Users\\User\\Desktop\\Back_end\\brain(iter-dr)\\b60-25"
+>>>>>>> linked to localhost
 nlp = spacy.load(input_dir)
 
-sentences =[
-    "They are considering to get a JCL Personal loan",
-    "personal loan, car loan, house loans, personal financing, business loan",
-    "personal loans, car loans, house loans, personal financings, business loans",
-    "Singapore officially the Republic of Singapore is an island city-state in Southeast Asia.",
-    "personal loans, car loans, house loans, housing loans, personal financings, business loan",
-    "Islamic personal loan",
-    "Apple is looking at buying U.K. startup for $1 billion",
-    "Jack is searching for the best interest rate for small business loans",
-    "Compare Malaysian housing loans with our housing loan calculator.",
-    "for a new mortgage loan or refinance your mortgage with Citibank's competitive mortgage loan",
-    "Donald John Trump (born June 14, 1946) is the 45th and current president of the United States.",
-    "Get the latest news, updates, and happenings at Google.",
-    "He is in need of some personal financing advice"
-    ]
+# sentences =[
+#     "They are considering to get a JCL Personal loan",
+#     "personal loan, car loan, house loans, personal financing, business loan",
+#     "personal loans, car loans, house loans, personal financings, business loans",
+#     "Singapore officially the Republic of Singapore is an island city-state in Southeast Asia.",
+#     "personal loans, car loans, house loans, housing loans, personal financings, business loan",
+#     "Islamic personal loan",
+#     "Apple is looking at buying U.K. startup for $1 billion",
+#     "Jack is searching for the best interest rate for small business loans",
+#     "Compare Malaysian housing loans with our housing loan calculator.",
+#     "for a new mortgage loan or refinance your mortgage with Citibank's competitive mortgage loan",
+#     "Donald John Trump (born June 14, 1946) is the 45th and current president of the United States.",
+#     "Get the latest news, updates, and happenings at Google.",
+#     "He is in need of some personal financing advice"
+#     ]
 
 # for s in sentences:
     # print(s)
@@ -29,4 +33,9 @@ def return_matches(input):
     for ent in doc.ents:
         print(doc, ent.text, ent.start_char, ent.end_char, ent.label_)
         storage.append(ent.text)
+<<<<<<< HEAD
     return storage
+=======
+    return storage  
+
+>>>>>>> linked to localhost
